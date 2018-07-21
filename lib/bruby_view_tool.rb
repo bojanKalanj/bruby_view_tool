7973b0e84d64922beeac765ec9bb8f7d0ecb60e8
bojanKalanj <bojan.kalanj@gmail.com>
2018-07-21 16:23:16 +0200

    Inital commit

---
+++ lib/bruby_view_tool.rb
@@ -0,0 +1,6 @@
+require "bruby_view_tool/version"
+require "bruby_view_tool/renderer"
+
+module BrubyViewTool
+  # Your code goes here...
+end
---
+++ lib/bruby_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BrubyViewTool
+  VERSION = "0.1.0"
+end
